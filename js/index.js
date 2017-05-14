28a353ba8d245f7221cbc59a98344cefeafa5a6e
Stanislav Bronevytskyy <ssbron2@gmail.com>
2017-05-14 20:00:48 +0000

    Add changes

---
+++ js/index.js
@@ -0,0 +1,34 @@
+alert("This is a true magic");
+var a = Number(prompt("please enter any number you want to know in Fibonacci sequence"));
+
+if (a === 0){
+    document.write('in classic Fibonacci sequence we do not have 0. please start with 1');
+}
+else {
+    var result = Fibonacci(a);
+    document.write(result);
+}
+
+
+// function Fibonacci(a){
+//   if (a === 1){
+//   return 1;
+//   }
+//     if (a === 2){
+//   return 1;
+//     }
+//     return Fibonacci(a-1)+Fibonacci(a-2);
+
+// }
+
+
+function Fibonacci(a){
+    (a === 1){
+   return 1;
+   }
+    if (a === 2){
+   return 1;
+    }
+    for (a=1; a>0; (a-1)+(a-2
+
+}
